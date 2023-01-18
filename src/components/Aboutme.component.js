e5086b7a110ab30136bc458fc4a5af2a2f5751d7
bconti123 <bconti00@gmail.com>
2023-01-18 15:11:04 -0800

    Updated

--- src/components/Aboutme.component.js
+++ src/components/Aboutme.component.js
@@ -13,10 +13,11 @@ export default class Aboutme extends Component {
                   <div className="row">
                     <div className="col-md-6">
                       <h3>Passion</h3>
-                      <p>I always learn many things about technology by researching and discovering software, hardware, and programming languages. Usually, I always fail in many attempts until I succeed to gain knowledge and improve skills to become a fast hands-on project and quick problem solver.</p>
-                      <p>Cloud computing became my passion when I learned how to manage Amazon EC2 and EBS first time to backup storage and database from the CIT System Management class at California State University, Northridge. I love learning a lot about cloud computing. I continue to study more about AWS and work on hands-on experience.
+                      <p>I have always been interested in technology and enjoy researching and discovering new software, hardware, and programming languages. Though I have faced challenges and failed in many attempts, I have persisted and gained knowledge and improved my skills to become a fast hands-on problem solver.</p>
+                      <p>Cloud computing, in particular, has become my passion. I first learned how to manage Amazon EC2 and EBS in a CIT System Management class at California State University, Northridge. I continue to study more about AWS and gain hands-on experience.
                       </p>
-                      <p>I have volunteer work as Web Coordinator for Bay Area Association Deaf Asian (BAADA). I have responsibility for developing, designing, and implementing a website by testing and deploying it through Wix as a web designer framework. I attend meetings with the president, vice president, and board members to discuss more website improvement and add creative ideas to keep the website updated and meet the requirement utilizing agile development as planning, strategy, and communication.</p>
+                      <p>I also volunteer as the Web Coordinator for the Bay Area Association Deaf Asian (BAADA). I am responsible for developing, designing, and implementing a website using the Wix framework. I attend meetings with the president, vice president, and board members to discuss website improvements and bring new ideas to keep the website updated, utilizing agile development for planning, strategy, and communication.
+                        </p>
                     </div>
                     <div className="col-md-6"> <img className="img-responsive" src="img/aboutpic.jpg" align="" alt="Bryant-Aboutme" /> </div>
 
